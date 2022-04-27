f711bab62d92bbd5aaf80f73ec9bf1a6b5466d2f
itscampa <mac.aerdna@gmail.com>
2022-04-27 12:00:45 +0200

    added echo to the counter

--- trufflist.sh
+++ trufflist.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 #check number of arguments
-if [ "$#" -ne 2 ] 
+if [ "$#" -lt 2 ] 
 then
     echo "Usage is ./trufflehog [--only-verified] <output_file.txt> <list_to_scan.txt>"
+    exit
 fi
 #go to the last argument and echo it
 for last; do true; done
@@ -16,26 +17,29 @@ echo "Output file: ${@: -2:1}"
 #traverse the array
 
 if [ "$1" == "--only-verified" ]
-    then
-        echo "Only verified data"
-        counter=0
-        while [ $counter -lt $noofelements ]
-        do
-            echo " Element $counter is  ${myarray[$counter]}" >> ${@: -2:1}
-            ./trufflehog --only-verified github --org=${myarray[$counter]} >> ${@: -2:1}
-            counter=$(( $counter + 1 ))
-        done
-    else
-        echo "All data (not only verified)"
-        counter=0
-        while [ $counter -lt $noofelements ]
-        do
-            echo " Element $counter is  ${myarray[$counter]}" >> ${@: -2:1}
-            ./trufflehog github --org=${myarray[$counter]} >> ${@: -2:1}
-            counter=$(( $counter + 1 ))
-        done
-    fi
+then
+    echo "Only verified data"
+    counter=0
+    while [ $counter -lt $noofelements ]
+    do
+        echo "Element $counter is  ${myarray[$counter]}"
+        echo "Element $counter is  ${myarray[$counter]}" >> ${@: -2:1}
+        ./trufflehog --only-verified github --org=${myarray[$counter]} >> ${@: -2:1}
+        counter=$(( $counter + 1 ))
+    done
+else
+    echo "All data (not only verified)"
+    counter=0
+    while [ $counter -lt $noofelements ]
+    do
+        echo "Element $counter is  ${myarray[$counter]}"
+        echo "Element $counter is  ${myarray[$counter]}" >> ${@: -2:1}
+        ./trufflehog github --org=${myarray[$counter]} >> ${@: -2:1}
+        counter=$(( $counter + 1 ))
+    done
+fi
 
 
 #last ${@: -2:1}
 #second to last ${@: -1}
+#still need to source the trufflehog binary automatically
