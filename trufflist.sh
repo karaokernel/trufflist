46741186955f1b03c817255facf8f02f6b4c5038
itscampa <mac.aerdna@gmail.com>
2022-04-27 11:46:00 +0200

    added argument checks

--- trufflist.sh
+++ trufflist.sh
@@ -1,12 +1,41 @@
 #!/usr/bin/env bash
-myarray=(`cat $1`)
-noofelements=${#myarray[*]}
-#now traverse the array
-counter=0
-while [ $counter -lt $noofelements ]
-do
-    echo " Element $counter is  ${myarray[$counter]}" >> report.txt
-    ./trufflehog $2 github --org=${myarray[$counter]} >> report.txt
-    counter=$(( $counter + 1 ))
 
-done
+#check number of arguments
+if [ "$#" -ne 2 ] 
+then
+    echo "Usage is ./trufflehog [--only-verified] <output_file.txt> <list_to_scan.txt>"
+fi
+#go to the last argument and echo it
+for last; do true; done
+echo "List to scan: ${@: -1}"
+#save file with name $last in myarray
+myarray=(`cat $last`)
+noofelements=${#myarray[*]}
+#output arguments selected 
+echo "Output file: ${@: -2:1}"
+#traverse the array
+
+if [ "$1" == "--only-verified" ]
+    then
+        echo "Only verified data"
+        counter=0
+        while [ $counter -lt $noofelements ]
+        do
+            echo " Element $counter is  ${myarray[$counter]}" >> ${@: -2:1}
+            ./trufflehog --only-verified github --org=${myarray[$counter]} >> ${@: -2:1}
+            counter=$(( $counter + 1 ))
+        done
+    else
+        echo "All data (not only verified)"
+        counter=0
+        while [ $counter -lt $noofelements ]
+        do
+            echo " Element $counter is  ${myarray[$counter]}" >> ${@: -2:1}
+            ./trufflehog github --org=${myarray[$counter]} >> ${@: -2:1}
+            counter=$(( $counter + 1 ))
+        done
+    fi
+
+
+#last ${@: -2:1}
+#second to last ${@: -1}
